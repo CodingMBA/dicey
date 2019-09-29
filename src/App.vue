f3809199b94ca1ec985268b815404f0eac9994e2
Andrew Smith <codingmba@gmail.com>
2019-09-29 16:55:30 -0400

    moves the play again dialog logic to a new component

--- src/App.vue
+++ src/App.vue
@@ -17,26 +17,11 @@
 
     <v-content>
       <Greeting />
-
-      <div class="text-center">
-        <v-dialog v-model="dialog" width="500">
-          <v-card>
-            <v-card-title class="headline grey lighten-2" primary-title>
-              {{ `#${currentRoll} is the Winner!!` }}
-            </v-card-title>
-            <v-card-text class="subtitle-1 blue--text text--darken-2 mt-4"
-              >Would you like to play again?</v-card-text
-            >
-            <v-divider></v-divider>
-            <v-card-actions>
-              <div class="flex-grow-1"></div>
-              <v-btn color="#0053a4" text @click="resetCount">Yes</v-btn>
-              <v-btn color="#D50000" text @click="dialog = false">No</v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-      </div>
-
+      <playAgainDialog
+        :currentRoll="currentRoll"
+        :dialog="dialog"
+        @reset="resetCount"
+      />
       <v-container id="play-area">
         <v-layout row justify-space-around align-center>
           <v-flex md3>
@@ -77,12 +62,14 @@
 <script>
 import Greeting from "./components/Greeting";
 import Scoreboard from "./components/Scoreboard";
+import playAgainDialog from "./components/playAgainDialog";
 
 export default {
   name: "App",
   components: {
     Greeting,
-    Scoreboard
+    Scoreboard,
+    playAgainDialog
   },
   data: () => {
     return {
